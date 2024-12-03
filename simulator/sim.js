cab7d97d07da56fb043241de0c0b8443e832a7f2
Oisin Hearne <oisinhearne@gmail.com>
2024-12-03 21:46:56 +0000

    Uploaded some research into running showdown locally and tensorflow agents.

---
+++ simulator/sim.js
@@ -0,0 +1,14 @@
+// TEMP CODE FROM https://github.com/smogon/pokemon-showdown/blob/master/sim/SIMULATOR.md
+
+const Sim = require('pokemon-showdown');
+stream = new Sim.BattleStream();
+
+(async () => {
+    for await (const output of stream) {
+        console.log(output);
+    }
+})();
+
+stream.write(`>start {"formatid":"gen7randombattle"}`);
+stream.write(`>player p1 {"name":"Alice"}`);
+stream.write(`>player p2 {"name":"Bob"}`);
